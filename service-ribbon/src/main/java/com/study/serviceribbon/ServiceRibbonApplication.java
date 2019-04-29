d2b168032dc545765e9ae571a048a25fb32a4399
swu <swu@joinhealth.cn>
2019-04-29 15:21:46 +0800

    Hystrix Dashboard 图形化数据监控

--- service-ribbon/src/main/java/com/study/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/study/serviceribbon/ServiceRibbonApplication.java
@@ -1,15 +1,19 @@
 package com.study.serviceribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ServiceRibbonApplication {
 
@@ -21,6 +25,24 @@ public class ServiceRibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * Hystrix Dashboard 图形化数据监控
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+        return registrationBean;
     }
 
 }
